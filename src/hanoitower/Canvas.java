9f30f7c3f2ea20ed404a6189c699ffbc120df23e
levon <levonaloian@gmail.com>
2016-04-11 22:27:36 +0400

    add mousePress and mouseDraget methods

--- src/hanoitower/Canvas.java
+++ src/hanoitower/Canvas.java
@@ -5,15 +5,56 @@ import figures.Figure;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class Canvas extends JPanel implements Runnable {
-
+    private boolean isSelected;
+    private int mX;
+    private int mY;
 
     public Canvas() {
         super();
 
+        setBackground(Color.lightGray);
 
-     //TODO: add MouseListener and MouseMotionListener
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mousePress(e);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                mouseDraget(e);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
     }
 
 
@@ -34,7 +75,6 @@ public class Canvas extends JPanel implements Runnable {
        * * *
       * * * *
  */
-
 
 
     }
@@ -75,10 +115,18 @@ public class Canvas extends JPanel implements Runnable {
     }
 
     private void mousePress(MouseEvent e) {
-
+        mX = e.getX();
+        mY = e.getY();
+        select(mX, mY);
+        update(getGraphics());
     }
 
     private void mouseDraget(MouseEvent e) {
-
+        if (isSelected) {
+            getSelected().move(e.getX() - mX, e.getY() - mY);
+            repaint();
+        }
+        mX = e.getX();
+        mY = e.getY();
     }
 }
